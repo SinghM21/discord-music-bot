c3759da42d866be1062e3dd0770b2b05ec42cf33
SinghM21 <72313996+SinghM21@users.noreply.github.com>
2024-01-14 22:40:16 +0000

    added skip functionality + some queue functionality

--- musicCog.py
+++ musicCog.py
@@ -7,6 +7,11 @@ import configparser
 class MusicCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.queue = []
+        self.config = configparser.RawConfigParser()
+        self.config.read('config.cfg')
+        self.config_dict = dict(self.config.items('DMSB_Config'))
+
 
     @commands.command(name='join')
     async def join(self, ctx):
@@ -19,30 +24,43 @@ class MusicCommands(commands.Cog):
     @commands.command(name='play')
     async def play(self, ctx, link: str):
         await MusicCommands.join(self, ctx)
+        
+        self.queue.append(link)
 
-        config = configparser.RawConfigParser()
-        config.read('config.cfg')
-        config_dict = dict(config.items('DMSB_Config'))
+        if not ctx.voice_client.is_playing():
+            await play_next(self, ctx)
+        else:
+            text_channel = self.bot.get_channel(ctx.channel.id)
+            await text_channel.send("Song added to queue")
 
-        yt = YouTube(link)
+    @commands.command(name='pause')
+    async def pause(self, ctx):
+        ctx.voice_client.pause()
+
+    @commands.command(name='resume')
+    async def resume(self, ctx):
+        ctx.voice_client.resume()
+
+    @commands.command(name='skip')
+    async def skip(self, ctx): 
+        await play_next(self, ctx)
+
+async def play_next(self, ctx):
+    if (len(self.queue) > 0):
+        yt = YouTube(self.queue[0])
         stream = yt.streams.get_by_itag(251)
-        stream.download(config_dict['file_path'], config_dict['audio_name'])
+        stream.download(self.config_dict['file_path'], self.config_dict['audio_name'], None, False)
 
         text_channel = self.bot.get_channel(ctx.channel.id)
         await now_playing(text_channel, yt.title)
 
-        voice_client = ctx.message.guild.voice_client
-        voice_client.play(discord.FFmpegPCMAudio(executable= config_dict['ffmpeg_exec'], source= os.path.join(config_dict['file_path'], config_dict['audio_name'])))
+        ctx.voice_client.stop()
+        ctx.voice_client.play(discord.FFmpegPCMAudio(executable= self.config_dict['ffmpeg_exec'], source= os.path.join(self.config_dict['file_path'], self.config_dict['audio_name']), after= lambda e: play_next(self, ctx)))
 
-    @commands.command(name='pause')
-    async def pause(self, ctx):
-        voice_client = ctx.message.guild.voice_client
-        voice_client.pause()
-
-    @commands.command(name='resume')
-    async def resume(self, ctx):
-        voice_client = ctx.message.guild.voice_client
-        voice_client.resume()
+        self.queue.pop(0)
+        
+    else:
+        ctx.voice_client.stop()
 
 async def now_playing(text_channel, title):
     await text_channel.send("Now playing: " + title)
